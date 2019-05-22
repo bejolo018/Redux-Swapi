721f5ac5014b0f9d439c593b7d81125bb1e3b50a
unknown <bejolo018@gmail.com>
2019-05-22 17:06:33 -0500

    completed action

--- src/actions/index.js
+++ src/actions/index.js
@@ -10,3 +10,13 @@ export const SUCCESS = 'SUCCESS';
 // our action creator will be a function that returns a function
 // the url to fetch characters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based action creator
+
+export const fetch = () => dispatch => {
+dispatch({ type: FETCHING });
+axios
+    .get('https://swapi.co/api/people')
+    .then(res =>
+        dispatch({ type: SUCCESS, payload: res.data.results})    
+    )
+    .catch(err => dispatch({ type: FAILURE, payload: err}));
+};
