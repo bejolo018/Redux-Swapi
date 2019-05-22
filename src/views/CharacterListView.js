59f1570e3ad73ad32bf3035d4925a4b5d655f896
unknown <bejolo018@gmail.com>
2019-05-22 17:22:20 -0500

    finished characterlistview, needs debugging

--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -2,7 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 
 import { CharacterList } from "../components";
-// import actions
+import { fetch } from '../actions';
 
 class CharacterListView extends React.Component {
   constructor() {
@@ -10,12 +10,13 @@ class CharacterListView extends React.Component {
   }
 
   componentDidMount() {
-    // call our action
+    this.props.fetch();
   }
 
   render() {
     if (this.props.fetching) {
       // return something here to indicate that you are fetching data
+      <div> loading... </div>
     }
     return (
       <div className="CharactersList_wrapper">
@@ -27,9 +28,18 @@ class CharacterListView extends React.Component {
 
 // our mapStateToProps needs to have two properties inherited from state
 // the characters and the fetching boolean
+
+const mapStateToProps = state => {
+  return{
+    characters: state.characters,
+    fetch: state.fetch,
+  }
+}
+
 export default connect(
-  null /* mapStateToProps replaces null here */,
+  mapStateToProps /* mapStateToProps replaces null here */,
   {
     /* action creators go here */
+    fetch
   }
 )(CharacterListView);
